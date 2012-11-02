f69079053b559f14d3d02d7b6e36997aa2b193fc
Henrik Schmidt-Møller <henrik.hsm@gmail.com>
2012-11-02 12:08:41 +0100

    Added Listing 3-2

--- src/main/java/cleancode/chap3/HtmlUtil.java
+++ src/main/java/cleancode/chap3/HtmlUtil.java
@@ -1,6 +1,8 @@
 package cleancode.chap3;
 
 public class HtmlUtil {
+
+    // Listing 3-1
     public static String testableHtml(
             PageData pageData,
             boolean includeSuiteSetup
@@ -55,5 +57,29 @@ public class HtmlUtil {
         }
         pageData.setContent(buffer.toString());
         return pageData.getHtml();
+    }
+
+    // Listing 3-2
+    public static String renderPageWithSetupsAndTeardowns(
+            PageData pageData, boolean isSuite
+    ) throws Exception {
+        boolean isTestPage = pageData.hasAttribute("Test");
+        if (isTestPage) {
+            WikiPage testPage = pageData.getWikiPage();
+            StringBuffer newPageContent = new StringBuffer();
+            includeSetupPages(testPage, newPageContent, isSuite);
+            newPageContent.append(pageData.getContent());
+            includeTeardownPages(testPage, newPageContent, isSuite);
+            pageData.setContent(newPageContent.toString());
+        }
+        return pageData.getHtml();
+    }
+
+    private static void includeSetupPages(WikiPage testPage, StringBuffer newPageContent, boolean suite) {
+        // ?
+    }
+
+    private static void includeTeardownPages(WikiPage testPage, StringBuffer newPageContent, boolean suite) {
+        // ?
     }
 }
